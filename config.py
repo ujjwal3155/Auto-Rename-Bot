52d0a5f350dc75c570a01803b855db038120abec
ujjwal3155 <153728856+ujjwal3155@users.noreply.github.com>
2024-03-04 16:08:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,16 +44,16 @@ Use These Keywords To Setup Custom File Name
 ✓ episode :- To Replace Episode Number
 ✓ quality :- To Replace Video Resolution
 
-<b>➻ Example :</b> <code> /autorename Naruto Shippuden S02 - EPepisode - quality  [Dual Audio] - @Madflix_Bots </code>
+<b>➻ Example :</b> <code> /autorename [TN] Naruto Shippuden [S02 - Eepisode] [quality] [Dual] @Otaku_Vision_M </code>
 
 <b>➻ Your Current Auto Rename Format :</b> <code>{format_template}</code> """
     
-    ABOUT_TXT = f"""<b>🤖 My Name :</b> <a href='https://t.me/Otaku_Vision_M'>Auto Rename Bot ⚡</a>
+    ABOUT_TXT = f"""<b>🤖 My Name :</b> <a href='https://t.me/Otaku_Vision_M'>Otaku Renamer ⚡</a>
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
 <b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
 <b>📢 Channel :</b> <a href='https://t.me/Otaku_Vision_M'>Otaku Vision</a>
-<b>🧑‍💻 Developer :</b> <a href='https://t.me/CallAdminRobot'>@aye_ujjwal</a>
+<b>🧑‍💻 Developer :</b> <a href='https://t.me/@aye_ujjwal'>@aye_ujjwal</a>
     
 <b>♻️ Bot Made By :</b> @aye_ujjwal"""
 
